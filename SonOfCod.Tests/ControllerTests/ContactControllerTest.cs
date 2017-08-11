ca343b0c9aea1830b7d1f14065ff57cff3e0b0be
Charles Emrich <libertinistic@gmail.com>
2017-08-11 13:58:36 -0700

    Begin building testing system

---
+++ SonOfCod.Tests/ControllerTests/ContactControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SonOfCod.Controllers;
+using SonOfCod.Models;
+using Xunit;
+
+namespace SonOfCod.Tests.ControllerTests
+{
+    public class ContactControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            ContactController controller = new ContactController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Get_ViewResult_Contacts_Test()
+        {
+            //Arrange
+            ContactController controller = new ContactController();
+
+            //Act
+            var result = controller.Contacts();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
