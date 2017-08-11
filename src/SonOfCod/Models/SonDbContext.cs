49e6b57c1177633426f66ae4cf6ffca6f0c79f05
Charles Emrich <libertinistic@gmail.com>
2017-08-11 12:40:59 -0700

    Initial commit, and import a bunch of code from prior user account management apps

---
+++ src/SonOfCod/Models/SonDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace SonOfCod.Models
+{
+    public class SonDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public SonDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public SonDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SonOfCod;integrated security=True");
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
